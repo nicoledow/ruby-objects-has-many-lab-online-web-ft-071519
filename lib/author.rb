3dd16bab8ccf9fe220832706ab6b3364bfd625e1
Nicole Dow <nikki.dow71@gmail.com>
2019-07-29 17:31:09 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,22 +1,5 @@
 class Author
-  attr_accessor :name, :author
-  @@all = []
   
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
   
-  def self.all
-    @@all
-  end
-  
-  def add_post(post)
-    post.author = self
-  end
-  
-  def posts
-    Post.all.select {|post| post.author == self}
-  end
   
 end
